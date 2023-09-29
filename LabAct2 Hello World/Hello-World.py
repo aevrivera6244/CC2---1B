9399b0c196e18cdd695e913736f9ec302505895c
aevrivera6244 <145944226+aevrivera6244@users.noreply.github.com>
2023-09-29 08:12:31 +0800

    Hello-World.py

---
+++ LabAct2 Hello World/Hello-World.py
@@ -0,0 +1,16 @@
+# Input Variables
+name = input("Your name: ")
+age_input = input("Your age:")
+age = int(age_input)
+age_next_year = age + 1
+
+# Aritmetic Ops
+pi = 3.1416
+modulo = 5 % 3 
+
+# Outputs or prints the variables
+print(f"GREETINGS, {name}")
+print(f"Your age next year is {age_next_year}")
+print(f"Value of Pi is {pi}")
+print(f"5 modulo 3 is {modulo}")
+print(f"5 times 3 is {5*3}")
